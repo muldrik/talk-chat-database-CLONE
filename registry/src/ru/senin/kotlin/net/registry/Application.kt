d5d8aa6c7b3801e64f889c831fb891d2221b182a
muldrik <muldrik@yandex.ru>
2021-02-17 02:02:15 +0300

    First version of a working database

--- registry/src/ru/senin/kotlin/net/registry/Application.kt
+++ registry/src/ru/senin/kotlin/net/registry/Application.kt
@@ -13,15 +13,23 @@ import io.ktor.network.sockets.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.utils.io.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.event.Level
 import ru.senin.kotlin.net.*
 import java.net.InetSocketAddress
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.concurrent.thread
+
 
 fun checkHttpUser(address: UserAddress): Boolean {
     val response = "http://${address.host}:${address.port}".httpGet()
@@ -89,7 +97,7 @@ interface DataProcessor {
     fun addUser(name: String, userAddress: UserAddress)
     fun deleteUser(name: String)
     fun isUserRegistered(name: String): Boolean
-    fun getUsersMap(): ConcurrentHashMap<String, UserAddress>
+    fun getUsersMap(): Map<String, UserAddress>
     fun clear()
     fun updateAttempts()
 }
@@ -112,7 +120,7 @@ class HashMapProcessor: DataProcessor {
         return users.containsKey(name)
     }
 
-    override fun getUsersMap(): ConcurrentHashMap<String, UserAddress> {
+    override fun getUsersMap(): Map<String, UserAddress> {
         return users
     }
 
@@ -133,12 +141,108 @@ class HashMapProcessor: DataProcessor {
                 deleteUser(user)
         }
     }
+}
+
+
+class SqlProcessor: DataProcessor {
+
+    object UserInfos : IntIdTable() {
+        val name = varchar("name", 50)
+        val protocol: Column<String> = varchar("protocol", 50)
+        val host:  Column<String> = varchar("host", 50)
+        val port: Column<Int> = integer("port")
+        val numberOfAttempts: Column<Int> = integer("number_of_attempts")
+    }
+
+    class UserInfo(id: EntityID<Int>) : IntEntity(id) {
+        companion object : IntEntityClass<UserInfo>(UserInfos)
+        var name by UserInfos.name
+        var protocol by UserInfos.protocol
+        var host by UserInfos.host
+        var port by UserInfos.port
+        var numberOfAttempts by UserInfos.numberOfAttempts
+    }
+
+    init {
+        Database.connect("jdbc:h2:./usersDatabase", driver = "org.h2.Driver")
+
+        transaction {
+            addLogger(StdOutSqlLogger)
+            SchemaUtils.create(UserInfos)
+        }
+    }
+
+    fun userToAddress(user: ResultRow): UserAddress {
+        val protocol = when(user[UserInfos.protocol]) {
+            "http" -> Protocol.HTTP
+            "ws" -> Protocol.WEBSOCKET
+            "udp" -> Protocol.UDP
+            else -> Protocol.HTTP
+        }
+        return UserAddress(protocol, user[UserInfos.host], user[UserInfos.port])
+    }
+
+    override fun addUser(name: String, userAddress: UserAddress) {
+        transaction {
+            UserInfo.new {
+                this.name = name
+                protocol = userAddress.protocol.protocol
+                host = userAddress.host
+                port = userAddress.port
+                numberOfAttempts = 0
+            }
+        }
+    }
+
+    override fun deleteUser(name: String) {
+        transaction {
+            UserInfos.deleteWhere { UserInfos.name eq name }
+        }
+    }
+
+    override fun isUserRegistered(name: String): Boolean {
+        var result = false
+        transaction {
+            result = (UserInfos.select { UserInfos.name eq name }.count() > 0)
+        }
+        return result
+    }
+
+    override fun getUsersMap(): Map<String, UserAddress> {
+        lateinit var result: Map<String, UserAddress>
+        transaction {
+            println(UserInfos.selectAll().associateBy({it[UserInfos.name]}, {userToAddress(it)}).toString())
+            result = UserInfos.selectAll().associateBy({it[UserInfos.name]}, {userToAddress(it)})
+        }
+        return result
+    }
+
+    override fun clear() {
+        transaction {
+            UserInfos.deleteAll()
+        }
+    }
+
+    override fun updateAttempts() {
+        transaction {
+            UserInfos.selectAll().forEach { user ->
+                user[UserInfos.numberOfAttempts].let {
+                    if (!checkUser(userToAddress(user)))
+                        user[UserInfos.numberOfAttempts] = it + 1
+                    else
+                        user[UserInfos.numberOfAttempts] = 0
+                }
+            }
+            UserInfos.deleteWhere { UserInfos.numberOfAttempts greater 3 }
+        }
+    }
 
 }
 
-val Registry: DataProcessor = HashMapProcessor()
+var Registry: DataProcessor = SqlProcessor()
 
 fun main(args: Array<String>) {
+
     thread {
         while (true) {
             Thread.sleep(1000 * 120)
@@ -149,12 +253,14 @@ fun main(args: Array<String>) {
     EngineMain.main(args)
 }
 
-
-
-
 @Suppress("UNUSED_PARAMETER")
 @JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    //val database: String = environment.config.property("database").getString()
+    /*Registry = when(database) {
+        "sql" -> TODO()
+        else -> HashMapProcessor()
+    }*/
     install(CallLogging) {
         level = Level.INFO
         filter { call -> call.request.path().startsWith("/") }
